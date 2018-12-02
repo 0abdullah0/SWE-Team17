1327e754107103865e663131b69db7cbb795f254
Mohamed Anwar <m.anwar@stud.fci-cu.edu.eg>
2018-12-02 21:09:46 +0200

    Revert "Update ShiftArray to take char_arr"

--- src/Team17/ShiftArray.java
+++ src/Team17/ShiftArray.java
@@ -5,32 +5,14 @@ package Team17;
  * @author Youssef Khaled
  */
 public class ShiftArray {
-    public void ShiftArray(int []arr, char []arrC)
+    public void ShiftArray(int []arr)
     {
         int tmp = arr[0];
-        char tmpC= arrC[0];
         for (int i = 0; i < arr.length-1; i++)
         {
             arr[i] = arr[i+1];
-            arrC[i] = arrC[i+1];
         }
         arr[arr.length-1] = tmp;
-        arrC[arr.length-1] = tmpC;
-        // Printing Arrays
-        System.out.print("The integer array after shift: ");
-        for( int i = 0; i < int_arr.length; i++ )
-        {
-            System.out.print(int_arr[i]+" ");
-        }
-        System.out.print("\nThe charcter array after shift: ");
-        for( int i = 0; i < char_arr.length; i++ )
-        {
-            System.out.print(char_arr[i]+" ");
-        }
-        System.out.println();
-   
     }
-        
-    
     
 }
